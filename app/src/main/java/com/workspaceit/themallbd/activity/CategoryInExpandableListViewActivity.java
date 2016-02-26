2fdb60991e6d0dc2a528488156b32ce1835fd840
Rajib Hossen <rajib@workspaceit.com>
2016-02-26 15:59:08 +0600

    expandable list view 20%

---
+++ app/src/main/java/com/workspaceit/themallbd/activity/CategoryInExpandableListViewActivity.java
@@ -0,0 +1,15 @@
+package com.workspaceit.themallbd.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.workspaceit.themallbd.R;
+
+public class CategoryInExpandableListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_in_expandable_list_view);
+    }
+}
